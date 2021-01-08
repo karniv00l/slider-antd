cf353810d3c6b29c652c906b04c957ac6ce3a4d2
Piotr Rogowski <piotr.rogowski0@gmail.com>
2021-01-08 19:57:57 +0100

    Initial commit 🚀

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Slider, Space } from 'antd';
+import { useState } from 'react';
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  const sliders = (count) => {
+    const result = [];
+
+    for (let i = 0; i < count; i++) {
+      result.push(<Slider
+        defaultValue={Math.random() * 100}
+        min={0}
+        max={100}
+        tooltipVisible
+        tooltipPlacement="bottom"
+      />);
+    }
+
+    return result;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ padding: 50 }}>
+        <Space direction="vertical" style={{ width: '80%' }}>
+          <Slider
+            value={number}
+            min={0}
+            max={15}
+            tipFormatter={(val) => `number of sliders: ${val}`}
+            tooltipVisible
+            onChange={(val) => setNumber(val)}
+          />
+          {sliders(number)}
+        </Space>
+      </div>
     </div>
   );
 }
